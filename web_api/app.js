c041602e8683fccb46942e1104fd047d13f55533
Ubuntu <biozahard@dev.zkxajyygcfyebpn4be4jckcfvg.ax.internal.cloudapp.net>
2017-06-11 09:19:55 +0000

    try

--- web_api/app.js
+++ web_api/app.js
@@ -11,6 +11,49 @@ web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 
 
+/**
+* CREATE Contract
+*/
+app.post('/contract', function (req, res) {
+
+
+
+
+//    web3.eth.accounts
+    code = fs.readFileSync('./../airlab/contracts/token/TokenEmission.sol').toString()
+    contract = web3.eth.compile.solidity(code); //contract.code: contract.info.abiDefinition:
+
+
+
+
+    EnergyContract = web3.eth.contract(contract.info.abiDefinition); //Application Binary Interface
+//    deployedContract = EnergyContract.new(['estate_name1','estate_name2','estate_name3', web3.eth.accounts[0], web3.eth.accounts[0], web3.eth.accounts[0], web3.eth.accounts[2]],{data: contract.code, from: web3.eth.accounts[0], gas: 4700000})
+
+
+    deployedContract = EnergyContract.new('kryptoTest', 'krt', 0, 1000)
+
+
+    console.log('deployedContract', deployedContract)
+
+
+    contractInstance = EnergyContract.at(deployedContract.address);
+
+
+    console.log('contractInstance', contractInstance)
+
+
+    console.log('deployedContract.address', deployedContract.address)
+
+
+    res.json({
+        charge_timestamp: '11111',
+        station_id: '11111',
+        device_id: '11111'
+    })
+
+
+})
+
 
 app.get('/balance', function (req, res) {
 
